c276d3c495b924ecd959da77c7e4a0f9c708e311
amine <aminedu38390@hotmail.fr>
2023-01-17 04:44:06 +0100

    recolor map

--- web/src/utils/recolormap.js
+++ web/src/utils/recolormap.js
@@ -1,8 +1,21 @@
 import * as d3 from 'd3'
 import { getRout } from './api'
 export default async function (country) {
-    // let res = getRout()
-    // d3.select('#map')
-    //     .selectAll('path')
-    //     .style("fill", 'black')
+
+    const color = d3.scaleLinear()
+        .domain([-10, 10])
+        .range(["red", "green"])
+
+    let res = await getRout('/api/event/map/color/' + country);
+    d3.select('#map')
+        .selectAll('path')
+        .style("fill", function (d) {
+            if (res[res.findIndex(item => item.actor2countrycode == d['properties']['ISO_A3'])]) {
+                console.log(res[res.findIndex(item => item.actor2countrycode == d['properties']['ISO_A3'])]);
+                return color(res[res.findIndex(item => item.actor2countrycode == d['properties']['ISO_A3'])].count)
+            }
+            if(d['properties']['ISO_A3'] == country) {
+                return 'yellow';
+            }
+        })
 }
